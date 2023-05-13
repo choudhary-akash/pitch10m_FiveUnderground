fa156c043659dabc25e61a769c9eaf60cca2d58d
jitndra <C_JitendraKumar.Patel@infinitylearn.com>
2023-05-14 04:55:15 +0530

    video api done

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,20 @@
-const express = require("express");
+const express = require('express');
+const bodyParser = require('body-parser');
+const route = require('./route');
+const mongoose = require('mongoose');
 const app = express();
 
-const PORT = process.env.PORT || 8000;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); 
 
-app.listen(PORT, () => {
-	console.log("Express server started...");
-	console.log(`Listening on port ${PORT}`);
+const mongoURL = "mongodb://akash:Akash%40123@139.59.54.108:27017/hackathon" 
+mongoose.connect(mongoURL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB is connected'))
+  .catch(err => console.log(err));
+
+app.use('/api', route);
+  
+ 
+app.listen(process.env.PORT || 8000, function () {
+  console.log('Express app running on port ' + (process.env.PORT || 8000));
 });
---
+++ backend/route.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const router = express.Router(); 
+const VideoModel = require('../backend/videoModel'); 
+ 
+
+// ============================ -- api -- =============================== //
+ 
+
+router.get("/videos",async function (req, res) {
+    try {  
+        let allData = await VideoModel.find().exec();
+        console.log(allData);
+        return res.status(200).send({ status: true, data: allData });
+    }
+    catch (error) {
+        return res.status(500).send({ status: false, msg: error.message });
+    }
+})
+ 
+router.get("/videos/:videoId",async function (req, res) {
+    try {  
+        let data = req.params.videoId;  
+        let allData = await VideoModel.findOne({ cmsVideoId:data}).exec(); 
+        return res.status(200).send({ status: true, data: allData });
+    }
+    catch (error) {
+        return res.status(500).send({ status: false, msg: error.message });
+    }
+})
+
+ 
+module.exports = router;
