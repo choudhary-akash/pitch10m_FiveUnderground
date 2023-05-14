e3862b4a428f44e45d10e3b7b43a92f0b52d5ae7
Akash Choudhary <akash.choudhary@infinitylearn.com>
2023-05-14 17:00:53 +0530

    UI & API changes

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require("cors");
 const bodyParser = require('body-parser');
 const route = require('./api/route');
 const mongoose = require('mongoose');
@@ -6,6 +7,8 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(cors());
+app.options('*', cors());
 
 const mongoURL = "mongodb://akash:Akash%40123@139.59.54.108:27017/hackathon" 
 mongoose.connect(mongoURL, { useNewUrlParser: true, useUnifiedTopology: true })
--- ui/src/pages/VideoView.jsx
+++ ui/src/pages/VideoView.jsx
@@ -1,13 +1,28 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import '../styles/VideoView.css'
 import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import {duration} from 'moment/moment';
 
 const VideoView = () => {
+	const [video, setVideo] = useState(null);
+	const videoRef = useRef(null);
 
-	let { cmsVideoId }= useParams();
-	console.log(cmsVideoId);
+	let { cmsVideoId } = useParams();
 
-	let videoUrl = document.location.host + `/api/videos/${cmsVideoId}`;
+	let getVideoAPI = "http://localhost:8000" + `/api/videos/${cmsVideoId}`;
+
+	useEffect(() => {
+		axios.get(getVideoAPI)
+		.then(res => {
+			setVideo(res.data.data);
+		});
+	}, []);
+
+	function jumpToTime(seconds) {
+		videoRef.current.currentTime = seconds;
+	}
+	
 
 	return (
 		<div id='video-view'>
@@ -16,12 +31,28 @@ const VideoView = () => {
 				Back
 			</button>		
 			<div className="video-container">
-				<video src="" controls></video>
+				<video src={video?.videoUrl || ''} controls ref={videoRef} autoPlay>
+					<track label='English' kind='subtitles' srcLang='en' src={video?.vttCaptionsUrl || ''} default />
+				</video>
 				<div className="timeline">
 					<div className='timeline-heading'>
 						<h3>Video Timeline</h3>
 					</div>
-					<div className='timeline-topics'></div>
+					<div className='timeline-topics'>
+						{
+							video?.timeline &&
+							video?.timeline?.map(timelineEl => 
+								<div className='timeline-topic'>
+									<span className='topic-name'>
+										{timelineEl.topic}
+									</span>
+									<span className='topic-timestamp' style={{cursor: 'pointer'}} onClick={() => jumpToTime(timelineEl.timestamp/1000)}>
+										{String(duration(timelineEl.timestamp, 'milliseconds').minutes()).padStart(2, '0') + ":" + String(duration(timelineEl.timestamp, 'milliseconds').seconds()).padStart(2, '0')}
+									</span>
+								</div>	
+							)
+						}
+					</div>
 					<div className='generate-qns-container'>
 						<button>Generate Questions</button>
 					</div>
